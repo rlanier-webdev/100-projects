bd015bd867ad6564a39da8917b77d13d26f71880
RaShunda Lanier <61757693+rlanier-webdev@users.noreply.github.com>
2021-02-14 17:43:25 -0500

    initial commit

---
+++ project7/js/scripts.js
@@ -0,0 +1,10 @@
+
+function processForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+
+    document.getElementById('output').innerHTML = message;
+}
+
